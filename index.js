d237eb7a12c9790add6ebc3d6e627cf44d58c983
OldHitman7 <farisbinasif7@gmail.com>
2022-07-02 17:07:18 +0500

    Missing semicolon xd

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2)
+image2.setAttribute("src", randomImageSource2);
 
 if(randomNumber1 > randomNumber2)
 {
